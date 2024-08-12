f8155ffd58416f8a586a860b456afc0b1aea260a
Reichel-eze <reicheleze@gmail.com>
2024-08-12 11:32:18 -0300

    TERMINADO EL EJERCICIO DE VIAJES

--- programa.pl
+++ programa.pl
@@ -34,8 +34,8 @@ tramo(15), escala(berlin,3), tramo(15), escala(tokyo,0)]).
 
 tiempoTotalVuelo(Vuelo, TiempoTotal) :-
     vuelo(Vuelo, _, Destinos),
-    findall(TiempoEscala, (member(escala(_, TiempoEscala), Destinos)), TiemposEscalas),
-    findall(TiempoTramo, (member(tramo(TiempoTramo), Destinos)), TiemposTramos),
+    findall(TiempoEscala, member(escala(_, TiempoEscala), Destinos), TiemposEscalas),
+    findall(TiempoTramo, member(tramo(TiempoTramo), Destinos), TiemposTramos),
     sum_list(TiemposEscalas, TotalEscalas),
     sum_list(TiemposTramos, TotalTramos),
     TiempoTotal is TotalEscalas + TotalTramos.
@@ -51,16 +51,109 @@ destino(Vuelo, Destino) :-      % relacion un vuelo con un destino (el Destino f
     vuelo(Vuelo, _, Destinos),
     member(Destino, Destinos).
     
-duracion(escala(_, Duracion), Duracion).
-duracion(tramo(Duracion), Duracion).    
+duracion(escala(_, TiempoDeEspera), TiempoDeEspera).
+duracion(tramo(TiempoEnVuelo), TiempoEnVuelo).    
 
-% escalaAburrida/2 : Relaciona un vuelo con cada una de sus escalas aburridas; una escala es aburrida si 
+% 2) escalaAburrida/2 : Relaciona un vuelo con cada una de sus escalas aburridas; una escala es aburrida si 
 % hay que esperar mas de 3 horas.
     
 escalaAburrida(Vuelo, Escala) :-
-    escalaDeVuelo(Vuelo, Escala),
-    duracion(Escala, Duracion),
-    Duracion > 3.
+    escalaDeVuelo(Vuelo, Escala),    % 1ero tengo que decir que es una escala!!
+    duracion(Escala, Duracion),      % uso duracion de arriba   
+    Duracion > 3.                    % para que sea aburrida
 
-escalaDeVuelo(Vuelo, escala(Ciudad, Tiempo)) :- destino(Vuelo, escala(Ciudad, Tiempo)).  % me dice las escalas que tiene un vuelo!!
+escalaDeVuelo(Vuelo, escala(Ciudad, Tiempo)) :- % me dice las escalas que tiene un vuelo!!
+    destino(Vuelo, escala(Ciudad, Tiempo)).  
 
+% 3) ciudadesAburridas/2 : Relaciona un vuelo con la lista de ciudades de sus escalas aburridas.
+
+ciudadesAburridas(Vuelo, Ciudades) :-
+    vuelo(Vuelo, _, _),
+    findall(Ciudad, (escalaAburrida(Vuelo, Escala), ciudadEscala(Escala, Ciudad)), Ciudades).
+
+ciudadEscala(escala(CiudadEscala, _), CiudadEscala).
+
+% 4) vueloLargo/1: Si un vuelo pasa 10 o más horas en el aire, 
+% entonces es un vuelo largo. OJO que dice "en el aire", en este punto 
+% no hay que contar las esperas en tierra. 
+
+vueloLargo(Vuelo) :-
+    tiempoTotalEnElAire(Vuelo, TiempoTotal),
+    TiempoTotal > 10.
+
+tiempoTotalEnElAire(Vuelo, TiempoEnElAire) :-
+    vuelo(Vuelo, _, _),
+    findall(Tiempo, (tramoDeVuelo(Vuelo, Tramo), duracion(Tramo, Tiempo)), Tiempos),
+    sum_list(Tiempos, TiempoEnElAire).
+    
+tramoDeVuelo(Vuelo, tramo(Tiempo)) :- % me dice los tramos que tiene un vuelo!!
+    destino(Vuelo, tramo(Tiempo)).
+
+% 4.1) conectados/2: Relaciona 2 vuelos si tienen al menos una ciudad en común.
+
+conectados(Vuelo, OtroVuelo) :-
+    pasaPorCiudad(Vuelo, Ciudad),
+    pasaPorCiudad(OtroVuelo, Ciudad),
+    Vuelo \= OtroVuelo.
+
+pasaPorCiudad(Vuelo, Ciudad) :-
+    escalaDeVuelo(Vuelo, Escala),   % la escala forma parte del vuelo   --> le podria poner destino(Vuelo, Destino), 
+    ciudadEscala(Escala, Ciudad).   % la ciudad de dicha escala         --> y despues que me diga la ciudad de ese destino ciudad(Destino, Ciudad).
+
+% 7) vueloLento/1: Un vuelo es lento si no es largo, y además cada escala es aburrida.
+
+vueloLento(Vuelo) :-
+    vuelo(Vuelo, _, _),
+    not(vueloLargo(Vuelo)),
+    forall(escalaDeVuelo(Vuelo, Escala), escalaAburrida(Vuelo, Escala)).
+    % para todoas las escalas del vuelo, son aburridas
+
+% 5) bandaDeTres/3: Relaciona 3 vuelos si están conectados, el primero
+% con el segundo, y el segundo con el tercero.
+
+bandaDeTres(Vuelo1, Vuelo2, Vuelo3) :-
+    conectados(Vuelo1, Vuelo2),
+    conectados(Vuelo2, Vuelo3),
+    Vuelo1 \= Vuelo3.
+
+% 6) distanciaEnEscalas/3: Relaciona dos ciudades que son escalas 
+% del mismo vuelo y la cantidad de escalas entre las mismas; si 
+% no hay escalas intermedias la distancia es 1. P.ej. 
+% - París y Berlín están a distancia 1 (por el vuelo BLE849), 
+% - Berlín y Seúl están a distancia 3 (por el mismo vuelo). 
+% No importa de qué vuelo, lo que tiene que pasar es que haya algún 
+% vuelo que tenga como escalas a ambas ciudades. Consejo: usar nth1
+
+% ME IMPORTA EL ORDEN DE LA LISTA!!
+
+distanciaEnEscalas(Ciudad1, Ciudad2, Distancia) :-      % EN ESTE NO VA A VER PROBLEMAS DE ORDEN!!
+    vuelo(Vuelo, _, Destinos),
+    pasaPorCiudad(Vuelo, Ciudad1),
+    pasaPorCiudad(Vuelo, Ciudad2),
+    nth1(M, Destinos, escala(Ciudad1, _)),
+    nth1(N, Destinos, escala(Ciudad2, _)),
+    findall(CiudadIntermedia,
+         (nth1(X, Destinos, escala(CiudadIntermedia, _)), entre(M,N,X)), CiudadesIntermedias),
+    length(CiudadesIntermedias, CantCiudadesIntermedias),
+    Distancia is 1 + CantCiudadesIntermedias.
+
+entre(Superior, Inferior, X) :-
+    Inferior < X, Superior > X.
+
+entre(Inferior, Superior, X) :-
+    Inferior < X, Superior > X.
+
+distanciaEnEscalasV2(Ciudad1, Ciudad2, Distancia) :-
+    vuelo(Vuelo, _, _),
+    pasaPorCiudad(Vuelo, Ciudad1),
+    pasaPorCiudad(Vuelo, Ciudad2),
+    %findall(Ciudad, member((escala(Ciudad, _)), Destinos), Ciudades),   % hago una lista de ciudades (puede hacer una funcion auxiliar abajo)
+    ciudadesVuelo(Vuelo, Ciudades),
+    nth1(M, Ciudades, Ciudad1), % obtengo la posicion de la primera ciudad
+    nth1(N, Ciudades, Ciudad2), % obtengo la posicion de la segunda ciudad
+    Distancia is abs(M - N).    % obtengo la distancia absoluta enntre las posiciones de ambas ciudades (no me importa el orden de como paso las ciudades)
+
+ciudadesVuelo(Vuelo, Ciudades) :-
+    vuelo(Vuelo, _, _),
+    findall(Ciudad, pasaPorCiudad(Vuelo, Ciudad), Ciudades).
+    
